618193f3cfc8f4dcf2f1f3f50cc3608c2a57a08e
Sayyad Amin <97822048+Sayyad-Amin@users.noreply.github.com>
2023-05-01 14:25:49 +0500

    initial commit

---
+++ dice_app/lib/main.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(dice_app());
+}
+class dice_app extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+        AppBar
+    );
+  }
+}
