f38c474c2ea0d24a3f8cee170b23e1f20ee31718
Sayyad Amin <97822048+Sayyad-Amin@users.noreply.github.com>
2023-05-01 18:42:04 +0500

    just added dices

--- dice_app/lib/main.dart
+++ dice_app/lib/main.dart
@@ -22,20 +22,74 @@ class dice_app  extends StatelessWidget {
 
 class dicePage  extends StatefulWidget {
 
+
+
   @override
   State<dicePage> createState() => _dice_appState();
 }
 class _dice_appState extends State<dicePage> {
+  var count=0;
+  void _incremnet(){
+    count+=1;
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Dice App'),
       ),
-      body: Container(
-        width: 100,
-        height: 100,
-        color: Colors.teal,
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Expanded(child:
+                    Image(image: AssetImage('assets/images/dice1.png'),)),
+                    SizedBox(width: 20),
+                    Expanded(child:
+                    Image(image: AssetImage('assets/images/dice2.png'),))
+                  ],
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Expanded(child:
+                    Image(image: AssetImage('assets/images/dice3.png'),)),
+                    SizedBox(width: 20),
+                    Expanded(child:
+                    Image(image: AssetImage('assets/images/dice4.png'),))
+                  ],
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Expanded(child:
+                    Image(image: AssetImage('assets/images/dice5.png'),)),
+                    SizedBox(width: 20),
+                    Expanded(child:
+                    Image(image: AssetImage('assets/images/dice6.png'),))
+                  ],
+                )
+              ],
+            ),
+          ),
+
+        ],
       ),
     );
   }
