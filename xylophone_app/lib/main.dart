54920e87699bc2e55311ef9f2f4ac0775735c49b
Sayyad Amin <97822048+Sayyad-Amin@users.noreply.github.com>
2023-05-01 07:11:50 +0500

    Just commited

--- xylophone_app/lib/main.dart
+++ xylophone_app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:audioplayers/audioplayers.dart';
+import 'package:assets_audio_player/assets_audio_player.dart';
 
 void main() => runApp(XylophoneApp());
 
