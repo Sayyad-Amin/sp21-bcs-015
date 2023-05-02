6cefee9a8410a661dfebf9e10233b115cda15d76
Sayyad Amin <97822048+Sayyad-Amin@users.noreply.github.com>
2023-05-02 09:52:10 +0500

    updated commit

--- dice_app/lib/main.dart
+++ dice_app/lib/main.dart
@@ -1,6 +1,9 @@
-
 import 'package:flutter/material.dart';
-
+import 'dart:math';
+int randomNumber=1;
+int left_image=1;
+int up_image=1;
+int down_image=1;
 void main(){
   runApp(dice_app());
 }
@@ -28,9 +31,18 @@ class dicePage  extends StatefulWidget {
   State<dicePage> createState() => _dice_appState();
 }
 class _dice_appState extends State<dicePage> {
-  var count=0;
-  void _incremnet(){
-    count+=1;
+
+  void random(){
+    randomNumber=Random().nextInt(5)+1;
+  }
+  void left(){
+    left_image=Random().nextInt(5)+1;
+  }
+  void up(){
+    up_image=Random().nextInt(5)+1;
+  }
+  void down(){
+    down_image=Random().nextInt(5)+1;
   }
   @override
   Widget build(BuildContext context) {
@@ -38,58 +50,76 @@ class _dice_appState extends State<dicePage> {
       appBar: AppBar(
         title: Text('Dice App'),
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    Expanded(child:
-                    Image(image: AssetImage('assets/images/dice1.png'),)),
-                    SizedBox(width: 20),
-                    Expanded(child:
-                    Image(image: AssetImage('assets/images/dice2.png'),))
-                  ],
-                )
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    Expanded(child:
-                    Image(image: AssetImage('assets/images/dice3.png'),)),
-                    SizedBox(width: 20),
-                    Expanded(child:
-                    Image(image: AssetImage('assets/images/dice4.png'),))
-                  ],
-                )
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    Expanded(child:
-                    Image(image: AssetImage('assets/images/dice5.png'),)),
-                    SizedBox(width: 20),
-                    Expanded(child:
-                    Image(image: AssetImage('assets/images/dice6.png'),))
-                  ],
-                )
-              ],
-            ),
-          ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Container(
+          decoration: BoxDecoration(borderRadius:
+          BorderRadius.circular(55),
+            color: Colors.teal,),
+          child: Column(
+            children: [
+              SizedBox(height: 100),
+              Column(
+                children: [
+                  SizedBox(
+                    width: 100,
+                    height: 100,
+                    child: FloatingActionButton(child: Image(image: AssetImage('assets/images/dice$up_image.png'),),
+                      onPressed: (){
+                        setState(() {
+                          up();
+                        });
+                      },),
+                  ),
+                  SizedBox(height: 50),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: SizedBox(
+                          width: 100,
+                          height: 100,
+                          child: FloatingActionButton(child: Image(image: AssetImage('assets/images/dice$left_image.png'),),
+                          onPressed: (){
+                            setState(() {
+                              left();
+                            });
+                          },),
+                        ),
+                      ),
+                      SizedBox(width: 40),
+                      Expanded(
+                        child: SizedBox(
+                          width: 100,
+                          height: 100,
+                          child: FloatingActionButton
+                          (child: Image(image: AssetImage('assets/images/dice$randomNumber.png'),),
+                          onPressed: (){
+                            setState(() {
+                              random();
+                            });
+                          },),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 50),
+                  SizedBox(
+                    width: 100,
+                    height: 100,
+                    child: FloatingActionButton(child: Image(image: AssetImage('assets/images/dice$down_image.png'),),
+                      onPressed: (){
+                        setState(() {
+                          down();
+                        });
+                      },),
+                  ),
+                ],
+              ),
 
-        ],
+
+            ],
+          ),
+        ),
       ),
     );
   }
